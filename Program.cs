6ea46685b68e0e5dafce295b79aa424e70e702fc
Robert Mair <48635807+RobertMair@users.noreply.github.com>
2020-04-28 12:16:53 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace galaxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theGalaxies = new List<Galaxy>()
+            {
+                new Galaxy("Tadpole", 400),
+                new Galaxy("Pinwheel", 25),
+                new Galaxy("Milky Way", 0),
+                new Galaxy("Andromeda", 3)
+            };
+
+            foreach (Galaxy i in theGalaxies)
+            {
+                Console.WriteLine("The " + i.Name + " galaxy is " + i.LightYears + " away!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Galaxy
+    {
+        public string Name { get; set; }
+        public int LightYears { get; set; }
+
+        public Galaxy(string name, int lightYears)
+        {
+            Name = name;
+            LightYears = lightYears;
+
+        }
+
+    }
+
+}
